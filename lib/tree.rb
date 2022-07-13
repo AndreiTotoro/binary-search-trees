616ab4bf41fb9f3f3b7e52d96a445b354ae345fe
AndreiTotoro <andreitotoro@gmail.com>
2022-07-13 18:54:58 +0300

    Fix the binary tree generator

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,26 +2,27 @@ require_relative 'node'
 
 class Tree
   def initialize(arr)
-    @array = arr
+    @data = arr.sort.uniq
+    @root = build_tree(@data)
   end
 
-  def build_tree(arr)
-    arr = arr.sort.uniq
-    low = 0
-    high = arr.length - 1
-    mid = (low + high) / 2
-    root = Node.new(arr[mid])
-    if mid == low
-      return
-    else
-      root.left_child = build_tree(arr[low..mid])
-    end
+  attr_reader :root
 
-    if mid == high
-      r
-    else
-      root.right_child = build_tree(arr[mid..high])
-    end
-    root
+  def build_tree(array)
+    return nil if array.empty?
+
+    middle = (array.size - 1) / 2
+    root_node = Node.new(array[middle])
+
+    root_node.left_child = build_tree(array[0...middle])
+    root_node.right_child = build_tree(array[(middle + 1)..-1])
+
+    root_node
+  end
+
+  def show_tree(node, prefix = '', is_left = true)
+    show_tree(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    show_tree(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
   end
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
 require_relative 'lib/tree'
 
 tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-puts tree.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree.show_tree(tree.root)
